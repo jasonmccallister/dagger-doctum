a15c16124e56cca48b073b521d08b6a16cd612ce
Jason McCallister <jason@dagger.io>
2025-02-14 14:06:09 -0500

    update excludes
    Signed-off-by: Jason McCallister <jason@dagger.io>

--- doctum-config.php
+++ doctum-config.php
@@ -10,7 +10,13 @@ $iterator = Finder::create()
     ->exclude("docker")
     ->exclude("runtime")
     ->exclude("tests")
-    ->exclude("src")
+    ->exclude("src/Codegen/")
+    ->exclude("src/Command/")
+    ->exclude("src/Connection/")
+    ->exclude("src/Exception/")
+    ->exclude("src/GraphQl/")
+    ->exclude("src/Service/")
+    ->exclude("src/ValueObject/")
     ->in("/work/repository/");
 
 return new Doctum($iterator);
